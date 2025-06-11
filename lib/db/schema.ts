85ff3675560ebc47bc93a4ec65535e08db50b0ce
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2025-06-11 14:01:44 +0530

    added drizzle config

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,6 +1,5 @@
 import { pgTable, text, uuid, integer, boolean, timestamp} from "drizzle-orm/pg-core"
 import { relations } from "drizzle-orm"
-import { Chilanka } from "next/font/google";
 
 export const files = pgTable("files", {
     id: uuid("id").defaultRandom().primaryKey(),
@@ -13,10 +12,10 @@ export const files = pgTable("files", {
     userId: text("user_id").notNull(), 
     parentId: uuid("parent_id"),
     isFolder: boolean("is_folder").default(false).notNull(), 
-  isStarred: boolean("is_starred").default(false).notNull(),
-  isTrash: boolean("is_trash").default(false).notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
 export const filesRelations = relations(files, ({ one, many}) => ({
