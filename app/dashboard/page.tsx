677254ecef7521feb5b34657f16abfc514dd4f05
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2025-06-16 21:02:00 +0530

    added file upload component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { CloudUpload } from "lucide-react";
+import Navbar from "@/components/Navabar";
 
 export default async function Dashboard(){
   const { userId } = await auth();
@@ -8,5 +10,22 @@ export default async function Dashboard(){
   if(!userId) {
     redirect("/sign-in")
   }
+
+   const serializedUser = user
+    ? {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        imageUrl: user.imageUrl,
+        username: user.username,
+        emailAddress: user.emailAddresses?.[0]?.emailAddress,
+      }
+    : null;
+
+    return (
+      <div className="min-h-screen flex flex-col bg-default-50" >
+        <Navbar user={serializedUser}/>
+      </div>
+    )
 
 }
