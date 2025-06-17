591a5e21d5e7fc31d0f2c0d978d9a080df7a4b9a
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2025-06-17 20:38:39 +0530

    UI Completed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { CloudUpload } from "lucide-react";
 import Navbar from "@/components/Navabar";
+import DashboardContent from "@/components/DashboardContent";
 
 export default async function Dashboard(){
   const { userId } = await auth();
@@ -25,7 +26,32 @@ export default async function Dashboard(){
     return (
       <div className="min-h-screen flex flex-col bg-default-50" >
         <Navbar user={serializedUser}/>
+        <main className="flex-1 container mx-auto py-8 px-6">
+        <DashboardContent
+          userId={userId}
+          userName={
+            user?.firstName ||
+            user?.fullName ||
+            user?.emailAddresses?.[0]?.emailAddress ||
+            ""
+          }
+        />
+      </main>
+       <footer className="bg-default-50 border-t border-default-200 py-6">
+        <div className="container mx-auto px-6">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="flex items-center gap-2 mb-4 md:mb-0">
+              <CloudUpload className="h-5 w-5 text-primary" />
+              <h2 className="text-lg font-bold">Droply</h2>
+            </div>
+            <p className="text-default-500 text-sm">
+              &copy; {new Date().getFullYear()} Droply
+            </p>
+          </div>
+        </div>
+      </footer>
       </div>
+      
     )
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 // tailwind.config.js
-const {heroui} = require("@heroui/theme");
+import {heroui} from "@heroui/theme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(avatar|button|card|divider|dropdown|input|modal|progress|toast|ripple|spinner|menu|popover|form).js"
+    "./node_modules/@heroui/theme/dist/components/(avatar|button|card|divider|dropdown|input|modal|progress|table|tabs|toast|popover|ripple|spinner|menu|form|checkbox|spacer).js"
 ],
   theme: {
-    extend: {},
+    extend: {
+       fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
